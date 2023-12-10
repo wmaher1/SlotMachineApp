2f84277939d2949b0cad93c1055ea371090bb546
wmaher1 <wmaher3@villanova.edu>
2023-12-10 17:44:13 -0500

    going back

--- src/App.js
+++ src/App.js
@@ -1,128 +1,131 @@
 import './App.css';
 import './slot-machine.css';
 import React from 'react';
-import { GoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
 
-(function SlotMachine() {
-  const items = [
-    '❄️',
-    '🍒',
-    '🎱',
-    '💰',
-    '💎',
-    '7️⃣',
-    '🃏',
-    '💣',
-    '🥇',
-    '🥊',    
-    '🍾',
-    '🍎',
-    '🧊',
-    '🔥',
-  ];
-  const doors = document.querySelectorAll('.door');
-  
-  document.querySelector('#spinner').addEventListener('click', spin);
-  document.querySelector('#reseter').addEventListener('click', init);
+const App = () => {
+  const { signedIn, signIn, signOut, user } = useGoogleLogin();
 
-  function init(firstInit = true, groups = 1, duration = 1) {
-    for (const door of doors) {
-      if (firstInit) {
-        door.dataset.spinned = '0';
-      } else if (door.dataset.spinned === '1') {
-        return;
-      }
 
-      const boxes = door.querySelector('.boxes');
-      const boxesClone = boxes.cloneNode(false);
-      const pool = ['❓'];
-
-      if (!firstInit) {
-        const arr = [];
-        for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
-          arr.push(...items);
-        }
-        pool.push(...shuffle(arr));
-
-        boxesClone.addEventListener(
-          'transitionstart',
-          function () {
-            door.dataset.spinned = '1';
-            this.querySelectorAll('.box').forEach((box) => {
-              box.style.filter = 'blur(1px)';
-            });
-          },
-          { once: true }
-        );
-
-        boxesClone.addEventListener(
-          'transitionend',
-          function () {
-            this.querySelectorAll('.box').forEach((box, index) => {
-              box.style.filter = 'blur(0)';
-              if (index > 0) this.removeChild(box);
-            });
-          },
-          { once: true }
-        );
-      }
-
-      for (let i = pool.length - 1; i >= 0; i--) {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.style.width = door.clientWidth + 'px';
-        box.style.height = door.clientHeight + 'px';
-        box.textContent = pool[i];
-        boxesClone.appendChild(box);
-      }
-      boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
-      boxesClone.style.transform = `translateY(-${door.clientHeight * (pool.length - 1)}px)`;
-      door.replaceChild(boxesClone, boxes);
-    }
-  }
-
-  async function spin() {
-    init(false, 1, 2);
+  const handlePlayGame = () => {
+    (function () {
+      const items = [
+        '❄️',
+        '🍒',
+        '🎱',
+        '💰',
+        '💎',
+        '7️⃣',
+        '🃏',
+        '💣',
+        '🥇',
+        '🥊',    
+        '🍾',
+        '🍎',
+        '🧊',
+        '🔥',
+      ];
+      const doors = document.querySelectorAll('.door');
+      
+      document.querySelector('#spinner').addEventListener('click', spin);
+      document.querySelector('#reseter').addEventListener('click', init);
     
-    for (const door of doors) {
-      const boxes = door.querySelector('.boxes');
-      const duration = parseInt(boxes.style.transitionDuration);
-      boxes.style.transform = 'translateY(0)';
-      await new Promise((resolve) => setTimeout(resolve, duration * 100));
-    }
-  }
+      function init(firstInit = true, groups = 1, duration = 1) {
+        for (const door of doors) {
+          if (firstInit) {
+            door.dataset.spinned = '0';
+          } else if (door.dataset.spinned === '1') {
+            return;
+          }
+    
+          const boxes = door.querySelector('.boxes');
+          const boxesClone = boxes.cloneNode(false);
+          const pool = ['❓'];
+    
+          if (!firstInit) {
+            const arr = [];
+            for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
+              arr.push(...items);
+            }
+            pool.push(...shuffle(arr));
+    
+            boxesClone.addEventListener(
+              'transitionstart',
+              function () {
+                door.dataset.spinned = '1';
+                this.querySelectorAll('.box').forEach((box) => {
+                  box.style.filter = 'blur(1px)';
+                });
+              },
+              { once: true }
+            );
+    
+            boxesClone.addEventListener(
+              'transitionend',
+              function () {
+                this.querySelectorAll('.box').forEach((box, index) => {
+                  box.style.filter = 'blur(0)';
+                  if (index > 0) this.removeChild(box);
+                });
+              },
+              { once: true }
+            );
+          }
+    
+          for (let i = pool.length - 1; i >= 0; i--) {
+            const box = document.createElement('div');
+            box.classList.add('box');
+            box.style.width = door.clientWidth + 'px';
+            box.style.height = door.clientHeight + 'px';
+            box.textContent = pool[i];
+            boxesClone.appendChild(box);
+          }
+          boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
+          boxesClone.style.transform = `translateY(-${door.clientHeight * (pool.length - 1)}px)`;
+          door.replaceChild(boxesClone, boxes);
+        }
+      }
+    
+      async function spin() {
+        init(false, 1, 2);
+        
+        for (const door of doors) {
+          const boxes = door.querySelector('.boxes');
+          const duration = parseInt(boxes.style.transitionDuration);
+          boxes.style.transform = 'translateY(0)';
+          await new Promise((resolve) => setTimeout(resolve, duration * 100));
+        }
+      }
+    
+      function shuffle([...arr]) {
+        let m = arr.length;
+        while (m) {
+          const i = Math.floor(Math.random() * m--);
+          [arr[m], arr[i]] = [arr[i], arr[m]];
+        }
+        return arr;
+      }
+    
+      init();
+    })();
 
-  function shuffle([...arr]) {
-    let m = arr.length;
-    while (m) {
-      const i = Math.floor(Math.random() * m--);
-      [arr[m], arr[i]] = [arr[i], arr[m]];
-    }
-    return arr;
-  }
 
-  init();
-})();
-
-
-function App() {
-
-  const responseMessage = (response) => {
-    console.log(response);
-  };
-  const errorMessage = (error) => {
-    console.log(error);
-  };
+    console.log("Playing the game");
+}
 
   return (
-    <div className="App">
-      <h2>Google Login</h2>
-      <br />
-      <br />
-      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+    <div>
+      {signedIn ? (
+        <div>
+          <p>Welcome, {user.name}!</p>
+          <button onClick={handlePlayGame}>Play Game</button>
+          <button onClick={signOut}>Sign Out</button>
+        </div>
+      ) : (
+        <button onClick={signIn}>Sign In with Google</button>
+      )}
     </div>
-      
   );
-}
+  };
 
 export default App;
